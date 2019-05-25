46521d28e20a3fad6ddf3666c426f8f6bab9ec8e
QinFanhua <qinfanhua@vip.qq.com>
2019-05-25 11:38:07 +0800

    First commit

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2019-05-25_00:11:00", 
+    "description": "为宽带提速而生！！！", 
+    "home_url": "Module_speedup.asp", 
+    "md5": "f62d2478339447e84ec892c29946b744", 
+    "module": "speedup", 
+    "title": "天翼云盘提速", 
+    "version": "2.1.1"
+}
